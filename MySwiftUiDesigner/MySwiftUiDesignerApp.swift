fbdf84a422cc8ffaba2365e000e9cd4995595d89
Arlak <arlak.a@orcsoft.co.th>
2023-04-21 16:23:21 +0700

    Initial Commit

---
+++ MySwiftUiDesigner/MySwiftUiDesignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUiDesignerApp.swift
+//  MySwiftUiDesigner
+//
+//  Created by Arlak Abdulloh on 21/4/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUiDesignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
